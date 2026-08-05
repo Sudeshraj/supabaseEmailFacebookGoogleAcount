09371bcce5871775c004fbdda1d7d66fa812328c
Sudeshraj <sudesh10raj@gmail.com>
2026-08-05 10:24:18 +0530

    Add Fixed NDK version for build gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -19,7 +19,8 @@ android {
     namespace = "com.example.flutter_application_1"
     //compileSdk = flutter.compileSdkVersion
     compileSdk = 36
-    ndkVersion = flutter.ndkVersion
+    //ndkVersion = flutter.ndkVersion
+    ndkVersion = "28.2.13676358"
 
     compileOptions {        
         sourceCompatibility = JavaVersion.VERSION_17
